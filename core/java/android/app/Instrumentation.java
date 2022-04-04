f9ba047292fd50d310e88b296d961e183721a91e
Danny Lin <danny@kdrag0n.dev>
2022-04-04 15:12:17 +0530

    core: Spoof build properties
    Author: Danny Lin <danny@kdrag0n.dev>
    Date:   Wed May 5 02:01:39 2021 -0700
    
        core: Spoof build properties
    
        SafetyNet's CTS profile attestation checks whether Build.MODEL matches
        that of the device's stock OS and checking whether it's a device that
        matched the CTS Profile.
    
        Spoof the model by setting Build.MODEL to "Pixel 5" for
        Google Play Services to help pass SafetyNet.
    
        REF: https://github.com/ProtonAOSP/android_frameworks_base/commit/b8adfb5d711c0381350901a40a693b0a85fb9d76
        Change-Id: Ic762d8853967bf78db614efaf46166e527cb397a
        Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>
    
    Author: SamarV-121 <samarvispute121@pm.me>
    Date:   Tue Mar 15 05:08:39 2022 +0000
    
        Moarr spoofs
    
        * Spoof Pixel XL Model for google photos for unlimited goolag photos backup storage
        * Spoof walley fingerprint for gms package to pass sn
          - also remove redundant MODEL spoof for gms package
        * Spoof device to Pixel 5 for Goolag app for weather widgets
        * Spoof build and manufacturer for samsung apps
          - otherwise samsung apps will crash on samsung devices running aosp
    
        Change-Id: I2e06d4f94c1a0606a8750c2919d739d65bbe59d5
    
    Change-Id: Ieba0bdefa77cd876a5f8f2a63126247ae0a74522

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -41,6 +41,7 @@ import android.os.Process;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
+import android.os.SystemProperties;
 import android.os.TestLooperManager;
 import android.os.UserHandle;
 import android.util.AndroidRuntimeException;
@@ -60,6 +61,7 @@ import com.android.internal.content.ReferrerIntent;
 import java.io.File;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
@@ -1231,6 +1233,7 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
+        maybeSpoofBuild(app);
         return app;
     }
     
@@ -1248,7 +1251,52 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
+        maybeSpoofBuild(app);
         return app;
+    }
+
+    private static void setBuildField(String packageName, String key, String value) {
+        /*
+         * This would be much prettier if we just removed "final" from the Build fields,
+         * but that requires changing the API.
+         *
+         * While this an awful hack, it's technically safe because the fields are
+         * populated at runtime.
+         */
+        try {
+            // Unlock
+            Field field = Build.class.getDeclaredField(key);
+            field.setAccessible(true);
+
+            // Edit
+            field.set(null, value);
+
+            // Lock
+            field.setAccessible(false);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            Log.e(TAG, "Failed to spoof Build." + key + " for " + packageName, e);
+        }
+    }
+
+    private static void maybeSpoofBuild(Application app) {
+        String packageName = app.getPackageName();
+
+        if ("com.google.android.apps.photos".equals(packageName)) {
+            setBuildField(packageName, "MODEL", "Pixel XL");
+        }
+
+        if ("com.google.android.googlequicksearchbox".equals(packageName)) {
+            setBuildField(packageName, "MODEL", "Pixel 5");
+        }
+
+        if (packageName.startsWith("com.samsung") || packageName.startsWith("com.sec")) {
+            setBuildField(packageName, "BRAND", "google");
+            setBuildField(packageName, "MANUFACTURER", "google");
+        }
+
+        if ("com.google.android.gms".equals(packageName)) {
+            setBuildField(packageName, "FINGERPRINT", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+        }
     }
 
     /**
